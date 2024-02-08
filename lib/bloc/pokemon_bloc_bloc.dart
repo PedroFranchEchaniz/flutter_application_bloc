dcdaa5a560ae6f8274dc58d4db379e8470025f45
franch.ecped23_trian <pedrofe.av@gmail.com>
2024-02-08 09:08:09 +0100

    correjido error

--- lib/bloc/pokemon_bloc_bloc.dart
+++ lib/bloc/pokemon_bloc_bloc.dart
@@ -7,23 +7,20 @@ part 'pokemon_bloc_event.dart';
 part 'pokemon_bloc_state.dart';
 
 class PokemonBlocBloc extends Bloc<PokemonBlocEvent, PokemonBlocState> {
-
   final PokemonRepository pokemonRepository;
 
-  PokemonBlocBloc(this.pokemonRepository): super(PokemonBlocInitial()){
+  PokemonBlocBloc(this.pokemonRepository) : super(PokemonBlocInitial()) {
     on<PokemonFetchEvent>(_onPokemonFetch);
   }
 
   void _onPokemonFetch(
-    PokemonFetchEvent event, Emitter<PokemonBlocState> emit)async{
-      try{
-        final pokemonList = await pokemonRepository.fetchPokemons();
-        emit(PokemonblockFetched(pokemonList)); //se capturan estados
-        return;
-
-      }on Exception catch(e){
-        emit(PokemonblocFetchError(e.toString()));
-      }
+      PokemonFetchEvent event, Emitter<PokemonBlocState> emit) async {
+    try {
+      final pokemonList = await pokemonRepository.fetchPokemons();
+      emit(PokemonblockFetched(pokemonList)); //se capturan estados
+      return;
+    } on Exception catch (e) {
+      emit(PokemonblocFetchError(e.toString()));
     }
-  )
+  }
 }
